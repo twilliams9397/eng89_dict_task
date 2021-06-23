fc322c82efb0e90ca8d0dace91d6542582712d44
Tom <Tom1@MacBook-Pro-2.local>
2021-06-23 16:10:52 +0100

    dict_task file updated

---
+++ dict_task.py
@@ -0,0 +1,15 @@
+story1 = {
+    "start": "the beginning of the story",
+    "middle": "the middle section of the story",
+    "end": "the conclusion of the story"
+}
+
+print(story1)
+print(type(story1))
+print(story1.keys())
+print(story1.values())
+print(story1["start"])
+print(story1["middle"])
+print(story1["end"])
+
+story1["hero"] = "Batman"
